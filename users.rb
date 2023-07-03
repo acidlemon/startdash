a73099cca9f3338e8426d2ba15c1d1500a2d7566
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2023-07-03 19:27:25 +0900

    add users.rb

---
+++ users.rb
@@ -0,0 +1,48 @@
+node[:users].each do |username|
+  additional_group = ["sudo"]
+  if node[:use_docker] then
+    additional_group.push("docker")
+  end
+  
+  user "create user #{user}" do
+    username username
+    shell "/bin/bash"
+    create_home true
+  end
+  
+  additional_group.each do |group|
+    execute "add #{group}" do
+      only_if "id #{username}"
+      not_if "getent group #{group} | grep #{username}"
+      command "usermod #{username} -aG #{group}"
+    end
+  end
+  
+  if node[:platform] == "darwin" then
+    homedir = "/Users/#{username}"
+  else
+    homedir = "/home/#{username}"
+  end
+  
+  directory "#{homedir}" do
+    mode "0755"
+    owner username
+    group username
+  end
+  directory "#{homedir}/.ssh" do
+    mode "0700"
+    owner username
+    group username
+  end
+  
+  http_reqeust "#{homedir}/.ssh/authorized_keys" do
+    url "https://github.com/#{username}.keys"
+    path "#{homedir}/.ssh/authorized_keys"
+    owner username
+    mode "0600"
+  end
+  
+end
+
+
+
